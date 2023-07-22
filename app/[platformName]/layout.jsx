d5b9fe478154e0cb13968934aefb241b9fa19142
PavelMedek <medek41@gmail.com>
2023-07-22 15:39:49 +0200

    fix

--- app/[platformName]/layout.jsx
+++ app/[platformName]/layout.jsx
@@ -1,15 +1,19 @@
 import MainNavbar from "@/components/MainNavbar";
 import { platforms } from "@/utils/data";
 
-export default function Layout({ children }) {
+export default function Layout({ children, params }) {
+  const shows = platforms.find((el) => el.name === params.platformName);
+
   return (
     <>
       <main className="flex">
         <div className="mr-3 h-screen sticky top-0 md:hidden">
           <MainNavbar platforms={platforms} />
         </div>
-        <div className="bg-[#1A161E] w-80 h-screen sticky top-0 md:hidden">
-          Ahoj
+        <div className="bg-[#1A161E] w-80 h-screen sticky top-0 md:hidden overflow-auto p-10">
+          {shows.series.map((el) => (
+            <div key={el.title}>{el.title}</div>
+          ))}
         </div>
         <div className="w-full">{children}</div>
       </main>
--- utils/data.js
+++ utils/data.js
@@ -3,21 +3,53 @@ export const platforms = [
     name: "netflix",
     color: "#B6060F",
     image: "/images/netflixlogo.png",
+    series: [
+      {
+        title: "THE BABY-SITTERS CLUB",
+        desc: "A group of friends launch a babysitting business",
+      },
+      {
+        title: "HEARTSTOPPER",
+        desc: "eens Charlie and Nick discover their unlikely friendship might be something more as they navigate school and young love",
+      },
+    ],
   },
   {
     name: "disney",
     color: "#171EFF",
     image: "/images/disneyluslogo.png",
+    series: [
+      {
+        title: "American Born Chinese",
+        desc: "Elephants, they're just like us! Except for their elongated proboscis, airtight memory, and (alleged) fear of mice. This four-episode docuseries follows the world's largest land mammals in various biomes to show us",
+      },
+      {
+        title: "Marvel's Moon Girl and Devil Dinosaur",
+        desc: "This playful animated series based on the Marvel Comics of the same name is aimed at a younger audience than the big blockbuster Marvel movies that pack the theaters, but it's good for the whole family. The story follows a 13-year-old girl genius living in New York's Lower East Side who creates ",
+      },
+    ],
   },
   {
     name: "hbomax",
     color: "#902BE8",
     image: "/images/hbomax.webp",
+    series: [
+      {
+        title: "Hra o trůny",
+        desc: "Hra o trůny je americký fantasy dramatický seriál kabelové televize HBO. Jeho tvůrci jsou David Benioff a D. B. Weiss. Předlohou je populární, dosud nedokončená fantasy sága Píseň ledu a ohně spisovatele George R. R. Martina, který se na seriálu rovněž podílel. Seriál je pojmenován podle první knihy v sérii.",
+      },
+    ],
   },
   {
     name: "primevideo",
     color: "#4C4C4C",
     image: "/images/primevideo.jpg",
+    series: [
+      {
+        title: "FLEABAG",
+        desc: "A dry-witted woman, known only as Fleabag, has no filter as she navigates life and love in London while trying",
+      },
+    ],
   },
 ];
 
