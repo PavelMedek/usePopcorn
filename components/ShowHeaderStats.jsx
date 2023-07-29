ad22e4fb01982083948682bf9da1d93e30db15a6
PavelMedek <medek41@gmail.com>
2023-07-29 09:46:49 +0200

    fix

--- components/ShowHeaderStats.jsx
+++ components/ShowHeaderStats.jsx
@@ -84,7 +84,7 @@ const ShowHeaderStats = () => {
     //   </div>
     // </div>
 
-    <div className="grid lg:grid-cols-2 xl:grid-cols-3 grid-cols-5 gap-5">
+    <div className="grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 grid-cols-5 gap-5">
       <div className="bg-red-700 flex justify-center items-center aspect-video rounded-xl">
         <FaPlay className="w-8 h-8" />
       </div>
