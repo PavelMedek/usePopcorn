1d45da939a4e6c2e20b479e68bb7b38f1d3ca55e
PavelMedek <medek41@gmail.com>
2023-08-01 16:55:13 +0200

    refactor platform page

--- app/[platformName]/page.jsx
+++ app/[platformName]/page.jsx
@@ -1,29 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { useRouter } from "next/navigation";
-import Image from "next/image";
+import { useEffect, useMemo, useState } from "react";
 
 import { platforms } from "@/lib/data";
-import Link from "next/link";
 import PlatformBillboard from "@/components/PlatformBillboard";
 import ShowList from "@/components/ShowList";
-import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 const Page = ({ params }) => {
-  const platform = platforms?.find((el) => el.name === params.platformName);
+  const { platformName } = params;
+  const platform = platforms?.find((el) => el.name === platformName) || {};
 
-  const { color, image, name, cover, series } = platform || {};
+  const { color, image, name, cover, series } = platform;
 
-  const shows = series;
-
+  const shows = useMemo(() => series || [], [series]);
   const router = useRouter();
-
   const [showsByCategory, setShowsByCategory] = useState([]);
 
   useEffect(() => {
     // Categorize shows by category
-    const categorizedShows = shows?.reduce((result, show) => {
+    const categorizedShows = shows.reduce((result, show) => {
       show.categories.forEach((category) => {
         const existingCategory = result.find(
           (cat) => cat.category === category
@@ -37,51 +42,43 @@ const Page = ({ params }) => {
       return result;
     }, []);
 
-    function shuffleArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
-    }
-
-    const test = categorizedShows.map((item) => {
-      return shuffleArray(item.shows);
+    categorizedShows.forEach((item) => {
+      shuffleArray(item.shows);
     });
 
     setShowsByCategory(categorizedShows);
   }, [shows]);
 
-  if (!platform || platforms.length === 0) {
-    return (
-      <div className="flex flex-col justify-center items-center min-h-screen text-7xl uppercase w-full p-16 md:py-16 md:px-5 gap-10">
-        <p>No Platform</p>
-        <button
-          onClick={() => router.back()}
-          className="bg-blue-500 py-4 w-36 rounded-md font-bold text-lg text-center uppercase sm:py-2"
-        >
-          Back
-        </button>
-      </div>
-    );
-  }
-
   return (
     <div className="w-full flex flex-col p-16 md:py-16 md:px-5 gap-10">
-      <PlatformBillboard cover={cover} />
-      <ShowList slice={true} shows={shows} name={name} type="All" />
-
-      {showsByCategory.map((categoryObj) => (
-        <div key={categoryObj.category}>
-          <ShowList
-            shows={categoryObj.shows}
-            name={name}
-            type={categoryObj.category}
-            random={true}
-            slice={true}
-          />
+      {platforms.length === 0 || !platform ? (
+        <div className="flex flex-col justify-center items-center min-h-screen text-7xl uppercase w-full p-16 md:py-16 md:px-5 gap-10">
+          <p>No Platform</p>
+          <button
+            onClick={() => router.back()}
+            className="bg-blue-500 py-4 w-36 rounded-md font-bold text-lg text-center uppercase sm:py-2"
+          >
+            Back
+          </button>
         </div>
-      ))}
+      ) : (
+        <>
+          <PlatformBillboard cover={cover} />
+          <ShowList slice={true} shows={shows} name={name} type="All" />
+
+          {showsByCategory.map(({ category, shows }) => (
+            <div key={category}>
+              <ShowList
+                shows={shows}
+                name={name}
+                type={category}
+                random={true}
+                slice={true}
+              />
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
