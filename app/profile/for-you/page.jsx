178059aeeb120a4f73ffad0dcbcf1d297ac6160e
PavelMedek <medek41@gmail.com>
2023-08-14 18:46:00 +0200

    fix title in for you page

--- app/profile/for-you/page.jsx
+++ app/profile/for-you/page.jsx
@@ -14,7 +14,7 @@ const ForYouList = () => {
 
   return (
     <div className="w-full flex flex-col p-16 md:py-16 md:px-5 gap-10">
-      <h1 className="uppercase text-5xl">My List</h1>
+      <h1 className="uppercase text-5xl">For You List</h1>
       <div className="grid lg:grid-cols-1 xl:grid-cols-2 grid-cols-4 gap-5 ">
         {filteredPlatforms.map((platform) =>
           platform.series.map((series) => (
